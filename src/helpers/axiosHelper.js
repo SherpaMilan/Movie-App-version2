167c588989de25a4e60a9ab528b534c8eb87b4fa
SherpaMilan <msherpa574@icloud.com>
2023-02-15 19:06:20 +1100

    https updated

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -1,12 +1,12 @@
-import axios from "axios"
+import axios from "axios";
 
 export const fetchData = async (movieName) => {
-  const apiEp = `http://www.omdbapi.com/?apikey=ea9fbbc9&t=${movieName}`
+  const apiEp = `https://www.omdbapi.com/?apikey=ea9fbbc9&t=${movieName}`;
 
   try {
-    const { data } = await axios.get(apiEp)
-    return data
+    const { data } = await axios.get(apiEp);
+    return data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
